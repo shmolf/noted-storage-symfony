e0ebd474b15ff6d8bb617b21ffd7af57f221297c
Nicholas Browning <shmolf@gmail.com>
2021-07-17 15:13:33 +0000

    fix(oauth): need to communicate to parent that it's ready for the pipe

--- assets/scripts/user/oauth-success.ts
+++ assets/scripts/user/oauth-success.ts
@@ -2,14 +2,24 @@ import M from 'materialize-css';
 import 'materialize-css/dist/css/materialize.css';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 
-let pipe: MessagePort;
-
-window.addEventListener('message', (event: MessageEvent) => pipe = event.ports[0], false);
+window.addEventListener('message', (event: MessageEvent) => sendTokens(event.ports[0]), false);
 
 window.addEventListener('DOMContentLoaded', () => {
   M.AutoInit();
 
-  const refreshToken = document.getElementById('refresh-token') as HTMLInputElement;
-  const accessToken = document.getElementById('access-token') as HTMLInputElement;
-  pipe.postMessage(JSON.stringify({ refreshToken, accessToken }));
+  // See about extracting the referrer from the original request w/in the backend. And passing that to the JS.
+  // In this way, can reference an explicit referrer rather than a `*`
+  window.parent.postMessage('ready', '*');
 });
+
+function sendTokens(pipe: MessagePort) {
+  const refreshTokenElem = document.getElementById('refresh-token') as HTMLInputElement;
+  const refreshToken = refreshTokenElem.value;
+  refreshTokenElem.parentElement?.removeChild(refreshTokenElem);
+
+  const accessTokenElem = document.getElementById('access-token') as HTMLInputElement;
+  const accessToken = accessTokenElem.value;
+  accessTokenElem.parentElement?.removeChild(accessTokenElem);
+
+  pipe.postMessage(JSON.stringify({ refreshToken, accessToken }));
+}
